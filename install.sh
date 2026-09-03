da1bf6a0f6ed80c1b2eb992c475ec800883be7ea
Rodrigo Lopes <dev.rodrigo.lopes@gmail.com>
2026-09-03 12:15:53 +0100

    build: delegate installation to Makefile

--- install.sh
+++ install.sh
@@ -1,8 +1,4 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-DEST="${1:-$HOME/bin}"
-mkdir -p "$DEST"
-go build -trimpath -o "$DEST/git-update" .
-echo "Installed: $DEST/git-update"
-echo "Run: git update /path/to/projects"
+exec make install
